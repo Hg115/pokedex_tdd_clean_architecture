425f30184aeed38b136182e8e1ff8a380d84a5da
Hg115 <hrgm465@gmail.com>
2023-04-28 19:57:53 -0400

    Trying to add pokemon to favorite list

--- lib/features/pokedex/presentation/widget/show_pokemon.dart
+++ lib/features/pokedex/presentation/widget/show_pokemon.dart
@@ -1,7 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedex_tdd_clean_architecture/core/database/database_pokemon.dart';
-import 'package:pokedex_tdd_clean_architecture/features/pokedex/data/models/pokemon_model.dart';
 
 import '../../domain/entities/pokemon.dart';
 
